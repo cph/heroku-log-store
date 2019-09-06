19c1d997433fe4c32d4a9fe034ae96a9c06864b8
Matthew Kobs <matt.kobs@cph.org>
2019-09-06 10:42:20 -0500

    [fix] Fixed migration adding processed flag (3m)

--- db/migrations/003_add_processed_flag_to_events.rb
+++ db/migrations/003_add_processed_flag_to_events.rb
@@ -1,10 +1,12 @@
 Sequel.migration do
-  transaction
+  no_transaction
 
   up do
     run <<-EOS
       ALTER TABLE events ADD COLUMN processed_at TIMESTAMP WITH TIME ZONE;
+    EOS
 
+    run <<-EOS
       CREATE INDEX CONCURRENTLY events_processed_at ON events(processed_at);
     EOS
   end
